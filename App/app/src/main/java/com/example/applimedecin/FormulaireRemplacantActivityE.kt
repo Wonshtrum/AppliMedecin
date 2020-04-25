dc125572708610002c946cb15f64b2ac16a8880b
Wonshtrum <pygmee64@gmail.com>
2020-04-25 16:22:02 +0200

    Remplacant form

--- App/app/src/main/java/com/example/applimedecin/FormulaireRemplacantActivityE.kt
+++ App/app/src/main/java/com/example/applimedecin/FormulaireRemplacantActivityE.kt
@@ -10,8 +10,28 @@ class FormulaireRemplacantActivityE : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulaire_remplacant_e)
-        boutonSuivantERemp.setOnClickListener {
 
+        boutonSuivantERemp.setOnClickListener {
+            val data = intent.extras
+
+            if (data != null) {
+                val res = DoAsync {
+                    RequestCatalog.saveDataRemplacant(
+                        "-1",
+                        data.getString("carteProFilename", ""),
+                        data.getString("cvFilename", ""),
+                        data.getString("description", ""),
+                        data.getString("kmMax", ""),
+                        data.getString("addresse", ""),
+                        data.getString("mail", ""),
+                        data.getString("tel", ""),
+                        data.getString("specialite", ""),
+                        data.getString("mdp", ""),
+                        data.getString("zoneGeo", "")
+                    )
+                }.waitUntil()
+                println(res)
+            }
             startActivity(Intent(this@FormulaireRemplacantActivityE,MainActivity::class.java))
         }
     }
